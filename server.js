d1df31e9c6d0936a8798681f314e4609d95a1174
Doston Adikhamov <adikhamov94@gmail.com>
2025-09-17 16:55:03 +0900

    build express web server

---
+++ server.js
@@ -0,0 +1,31 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// 1Kirish codelari
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2 Session codelari
+// 3 Views codelari
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing codelari
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success" });
+})
+
+app.get('/', function (req, res) {
+    res.render("harid");
+});
+
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function (){
+    console.log(`Server ${PORT}da muvaffatqilatli ishlayapti`);
+});
