d8248e4f4419facd0641bc89f1ed4573b1913b8d
Doston Adikhamov <adikhamov94@gmail.com>
2025-09-18 21:22:55 +0900

    feat:author added

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log('Error:', err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
 
 // 1Kirish codelari
 app.use(express.static("public"));
@@ -17,10 +28,16 @@ app.set("view engine", "ejs");
 app.post("/create-item", (req, res) => {
     console.log(req.body);
     res.json({ test: "success" });
-})
+});
+
 app.get('/', function (req, res) {
     res.render("harid");
 });
+
+app.get('/author', (req, res) => {
+    res.render("author", {user: user});
+});
+
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function (){
