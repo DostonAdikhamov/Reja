0bc1950f0dd98ee6f5100e83d9e3e3958185ae9b
Doston Adikhamov <adikhamov94@gmail.com>
2025-09-21 17:23:47 +0900

    feat: train qismida CALLBACK &  ASYNC funksiyalarni qo'lladik

--- train.js
+++ train.js
@@ -8,28 +8,74 @@ const list = [
     "Hozir dam oling, endi kech", // 60+
 ];
 
+//CALLBACK Functionlar
 function maslahatBering(a, callback) {
     if(typeof a !== "number") callback("Iltimos, raqam kiriting", null);
-        else if(a <= 20) callback(null, list[0]);
-        else if(a > 20 && a <= 30) callback(null, list[1]);
-        else if(a > 30 && a <= 40) callback(null, list[2]);
-        else if(a > 40 && a <= 50) callback(null, list[3]);
-        else if(a > 50 && a <= 60) callback(null, list[4]);
-        else {
-            setTimeout(function() {
-                callback(null, list[5]);
-            }, 5000);
-            
-        }
+    else if(a <= 20) callback(null, list[0]);
+    else if(a > 20 && a <= 30) callback(null, list[1]);
+    else if(a > 30 && a <= 40) callback(null, list[2]);
+    else if(a > 40 && a <= 50) callback(null, list[3]);
+    else if(a > 50 && a <= 60) callback(null, list[4]);
+    else {
+      setInterval(function () {
+        callback(null, list[5]);
+      }, 3000);
+    }        
 }
 
-console.log("Buyerdan o'tdi 0")
+console.log("Buyerdan o'tdi 0");
 maslahatBering(65, (err, data) => {
     if(err) console.log("ERROR:", err);
     else {
-        console.log("Jack Madan maslahat:", data);
+        console.log("Jack Ma aytadiki:", data);
     }
-    
 });
 
-console.log("Buyerdan yetib keldi 1")
+console.log("Shu yerga keldi 1");
+
+
+//ASYNC Functionlar
+// async function maslahatBering(a) {
+//     if(typeof a !== "number") throw new Error("Iltimos, raqam kiriting");
+//         else if(a <= 20) return list[0];
+//         else if(a > 20 && a <= 30) return list[1];
+//         else if(a > 30 && a <= 40) return list[2];
+//         else if(a > 40 && a <= 50) return list[3];
+//         else if(a > 50 && a <= 60) return list[4];
+//         else {
+//         return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve (list[5]);
+//         }, 5000);
+            
+//         });
+
+// setTimeout(function() {
+//     callback(null, list[5]);
+// }, 5000);
+//     }
+// }
+
+// then + catch metodlari orqali CALL qilish
+// console.log("Buyerdan o'tdi 0")
+// maslahatBering(25)
+//     .then((data) => {
+//        console.log("Jack Madan maslahat:", data);
+// })
+//     .catch((err) => {
+//         console.log("ERROR:", err);
+// });
+
+// // console.log("Buyerdan yetib keldi 1")
+
+// // async + await metodi orqali CALL qilish
+// async function run() {
+//     let javob = await maslahatBering(30);
+//     console.log(javob) 
+//     javob = await maslahatBering(75);
+//     console.log(javob);
+//     javob = await maslahatBering(42);
+//     console.log(javob);
+// }
+
+// run();
